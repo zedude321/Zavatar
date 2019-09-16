d966a65899f271cfad2bc4d9e8554dd879013417
Zedude <zedude@ZedudeIsCool.local>
2019-09-16 17:46:55 +0800

    Added Color Icons Bros

--- Avatar/JS/editor.js
+++ Avatar/JS/editor.js
@@ -1,21 +1,17 @@
 const categories = firebase.firestore().collection('categories');
 
-function createCategory(name, photoURL){
+function createCategory(name, photoURL) {
     var category = document.createElement('div');
-    category.addEventListener('click', function(){
+    category.addEventListener('click', function () {
         clicked(name);
     });
     category.className = 'item';
     category.style.backgroundImage = "url('" + photoURL + "')";
-    // category.src = photoURL;
     return category;
 }
-
-
-function clicked(name){
+function clicked(name) {
     alert(name);
 }
-
 categories.get()
     .then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
