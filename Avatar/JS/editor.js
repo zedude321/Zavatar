5c2f1de0bab39d0cdc43bff3227e5de987bc746f
Zedude <zedude@ZedudeIsCool.local>
2019-09-18 17:01:36 +0800

    Added a function to editor.js

--- Avatar/JS/editor.js
+++ Avatar/JS/editor.js
@@ -1,7 +1,6 @@
 const categories = firebase.firestore().collection('categories');
-
-function createCategory(name, photoURL) {
-    var category = document.createElement('div');
+function createItem(name, photoURL) {
+    let category = document.createElement('div');
     category.addEventListener('click', function () {
         clicked(name);
     });
@@ -9,18 +8,38 @@ function createCategory(name, photoURL) {
     category.style.backgroundImage = "url('" + photoURL + "')";
     return category;
 }
-function clicked(name) {
-    alert(name);
-}
+
 categories.get()
     .then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
-            var el = createCategory(doc.id, doc.data().gsURL);
+            let el = createItem(doc.id, doc.data().gsURL);
             document.getElementsByClassName('menu')[0].appendChild(el);
         });
     })
     .catch(function (error) {
         console.log("Error getting documents: ", error);
     });
+
+function clicked(name) {
+    document.getElementsByClassName('menu')[0].innerHTML = "";
+    categories.doc(name).get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            let items = doc.data();
+
+            Object.keys(items).forEach(function (key) {
+                console.log(key); // key
+                console.log(items[key]); // value
+                let el = createItem(key, items[key].gsURL);
+                document.getElementsByClassName('menu')[0].appendChild(el);
+            });
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
