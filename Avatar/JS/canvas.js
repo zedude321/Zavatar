1caf24a5d8e16470efc0695714f2be55e0bddef7
Zedude321 <zedude321@gmail.com>
2019-10-05 10:27:06 +0800

    dfkpfhg9;8tgp;iuofgu

--- Avatar/JS/canvas.js
+++ Avatar/JS/canvas.js
@@ -7,6 +7,7 @@ const male = new Image();
 male.src = "./SRC/Guy.png";
 
 function draw(acquired){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(male, 0, 50, canvas.width, canvas.height);
     Object.keys(acquired).forEach(function (key) {
         categories.doc(key).get().then(function (doc) {
